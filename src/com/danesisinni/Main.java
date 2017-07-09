6d08a9ce1fcfffa14e9dd3d3f4959b77dbd9d381
Dane Sisinni <dfsisinni@gmail.com>
2017-07-09 15:42:30 -0400

    - added file writing - added argument specification

--- src/com/danesisinni/Main.java
+++ src/com/danesisinni/Main.java
@@ -1,9 +1,38 @@
 package com.danesisinni;
 
+import lombok.val;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
 
     public static void main(String[] args) {
-        CSVEnumGenerator  generator = new CSVEnumGenerator("file.csv", "NAME");
-        System.out.println(generator.getEnum());
+        String csvFile = "file.csv";
+        String enumName = "NAME";
+        String enumFile = "file.java";
+
+        if (args.length == 3) {
+            csvFile = args[0];
+            enumName = args[1];
+            enumFile = args[2];
+        }
+
+        CSVEnumGenerator  generator = new CSVEnumGenerator(csvFile, enumName);
+        val enumData = generator.getEnum();
+        writeToFile(enumData, enumFile);
     }
+
+    public static void writeToFile(String data, String fileName) {
+        try {
+            val path = Paths.get(fileName);
+            Files.createFile(path);
+            Files.write(path, data.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
